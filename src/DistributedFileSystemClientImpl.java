d282f8ec967c0302f5d570351dd42a5f1dc68764
Matthew Sell <matthew.r.sell@gmail.com>
2016-06-01 12:51:58 -0700

    Implementation stubs for discussion

---
+++ src/DistributedFileSystemClientImpl.java
@@ -0,0 +1,19 @@
+/**
+ * Implementation of Distributed File System Client
+ *
+ */
+public class DistributedFileSystemClientImpl implements DistributedFileSystemClient {
+
+	@Override
+	public boolean invalidate() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean writeback() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
---
+++ src/DistributedFileSystemServerImpl.java
@@ -0,0 +1,19 @@
+/**
+ * Implementation of Distributed File System Server
+ *
+ */
+public class DistributedFileSystemServerImpl implements DistributedFileSystemServer {
+
+	@Override
+	public FileContents download(String clientIPName, String filename, String mode) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean upload(String clientIPName, String filename, FileContents contents) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
