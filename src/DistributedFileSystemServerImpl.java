53a1a59814bb6a673e1bb59db9fb9a73978c416e
Matthew Sell <matthew.r.sell@gmail.com>
2016-06-01 13:02:43 -0700

    Minimal server "main", just to get things started, also extending UnicastRemoteObject as specified in earlier lab work, though this may not be necessary in the end

--- src/DistributedFileSystemServerImpl.java
+++ src/DistributedFileSystemServerImpl.java
@@ -1,8 +1,19 @@
+import java.rmi.Naming;
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
 /**
  * Implementation of Distributed File System Server
  *
  */
-public class DistributedFileSystemServerImpl implements DistributedFileSystemServer {
+@SuppressWarnings("serial")
+public class DistributedFileSystemServerImpl extends UnicastRemoteObject implements DistributedFileSystemServer {
+
+	public static final String SERVER_RMI_SERVICE_NAME = "dfs_server";
+	
+	
+	// required no-args constructor
+	public DistributedFileSystemServerImpl() throws RemoteException {}
 
 	@Override
 	public FileContents download(String clientIPName, String filename, String mode) {
@@ -14,6 +25,29 @@ public class DistributedFileSystemServerImpl implements DistributedFileSystemSer
 	public boolean upload(String clientIPName, String filename, FileContents contents) {
 		// TODO Auto-generated method stub
 		return false;
+	}
+
+	public static void main(String[] args) {
+		
+		try {
+			
+			// should always instantiate via interface
+			DistributedFileSystemServer server = new DistributedFileSystemServerImpl(); 
+			
+			// register server process with RMI service directory
+			Naming.rebind(SERVER_RMI_SERVICE_NAME, server);
+			
+			
+		}
+		catch (Exception e) {
+			
+			System.err.println("Exception in main(): " + e.getMessage());
+			
+			// negative return code is an error in execution
+			System.exit(-1);
+			
+		}
+		
 	}
 
 }
