eadc1bd31a6cd5ff5d5a7d62e106f80b7f27403d
Matthew Sell <matthew.r.sell@gmail.com>
2016-06-03 22:01:52 -0700

    Added debug statements to FileServer

--- src/FileServer.java
+++ src/FileServer.java
@@ -20,6 +20,8 @@ import java.util.Map;
 @SuppressWarnings("serial")
 public class FileServer extends UnicastRemoteObject implements ServerInterface {
 
+	public static final boolean DEBUG_MODE = true;
+	
 	public static final String CLIENT_RMI_SERVICE_NAME = "fileclient";
 	private static final String RMI_URL_PREFIX = "rmi://localhost:";
 	
@@ -36,10 +38,13 @@ public class FileServer extends UnicastRemoteObject implements ServerInterface {
 		try {
 			
 			// should always instantiate via interface
+			if (DEBUG_MODE) System.out.println("Launching FileServer on port " + args[0]);
 			ServerInterface server = new FileServer(Integer.parseInt(args[0])); 
 			
 			// register server process with RMI service directory
-			Naming.rebind(RMI_URL_PREFIX + args[0] + "/" + RMI_SERVICE_NAME, server);
+			String RMIUrl = RMI_URL_PREFIX + args[0] + "/" + RMI_SERVICE_NAME;
+			if (DEBUG_MODE) System.out.println("RMI: Binding FileServer at: " + RMIUrl);
+			Naming.rebind(RMIUrl, server);
 			
 		}
 		catch (Exception e) {
@@ -74,6 +79,8 @@ public class FileServer extends UnicastRemoteObject implements ServerInterface {
 	@Override
 	public FileContents download(String clientIPName, String filename, String mode) {
 
+		if (DEBUG_MODE) System.out.println("Received download request from " + clientIPName + " for " + filename + " with mode " + mode);
+
 		// make sure the client is no longer associated with any files - it's requesting a new one
 		for (HostedFile file : hostedFiles.values()) {
 			file.deRegisterClient(clientIPName);
@@ -83,7 +90,10 @@ public class FileServer extends UnicastRemoteObject implements ServerInterface {
 		HostedFile file = getFile(filename);
 	
 		// valid filename?
-		if (file == null) return null;
+		if (file == null) {
+			if (DEBUG_MODE) System.out.println("Filename invalid! (returning NULL to client)");
+			return null;
+		}
 		
 		// any client may get the file in read mode
 		if (mode.equalsIgnoreCase(ServerInterface.READ_MODE)) {
@@ -99,14 +109,23 @@ public class FileServer extends UnicastRemoteObject implements ServerInterface {
 			boolean registerSuccess = file.registerOwner(clientIPName, port);
 			
 			if (registerSuccess) {
+				
+				if (DEBUG_MODE) System.out.println("Client registered to file successfully - returning file");
+
 				return file.getFileContents();
+
 			} else {
+				
+				if (DEBUG_MODE) System.out.println("Unable to register client, returning NULL");
+
 				return null;
+
 			}
 			
 		}
 		
 		// unrecognized mode
+		if (DEBUG_MODE) System.out.println("Mode [" + mode + "] not recognized by FileServer! Returning NULL");
 		return null;
 	
 	}
@@ -118,8 +137,16 @@ public class FileServer extends UnicastRemoteObject implements ServerInterface {
 	 */
 	public HostedFile getFile(String filename) {
 
+		if (DEBUG_MODE) System.out.println("Checking local cache for " + filename);
+		
 		// filename provided?
-		if (filename == null || filename.length() == 0) return null;
+		if (filename == null || filename.length() == 0) {
+			
+			if (DEBUG_MODE) System.out.println("No filename provided! Returning NULL");
+
+			return null;
+
+		}
 		
 		// in cache?
 		HostedFile returnFile = hostedFiles.get(filename);
@@ -127,12 +154,21 @@ public class FileServer extends UnicastRemoteObject implements ServerInterface {
 		// not in cache - retrieve and populate from filesystem
 		if (returnFile == null) {
 			
+			if (DEBUG_MODE) System.out.println("File not found in local cache, reading from filesystem");
+
 			returnFile = new HostedFile(filename);
 			
 			// was there a file by the specified name?
-			if (returnFile.getFileContents() == null || returnFile.getFileContents().get().length == 0) return null;
+			if (returnFile.getFileContents() == null || returnFile.getFileContents().get().length == 0) {
+				
+				if (DEBUG_MODE) System.out.println("File not found in filesystem! Returning NULL");
+
+				return null;
+
+			}
 			
 			// valid file - put into cache
+			if (DEBUG_MODE) System.out.println("File read from filesystem, placing into local cache");
 			hostedFiles.put(filename, returnFile);
 
 		}
@@ -144,20 +180,40 @@ public class FileServer extends UnicastRemoteObject implements ServerInterface {
 	@Override
 	public boolean upload(String clientIPName, String filename, FileContents contents) {
 
+		if (DEBUG_MODE) System.out.println("Received upload request from " + clientIPName + " for " + filename);
+
 		// get the referenced file
+		if (DEBUG_MODE) System.out.println("Getting cached copy of file");
 		HostedFile file = getFile(filename);
 	
 		// valid filename?
-		if (file == null) return false;
+		if (file == null) {
+			if (DEBUG_MODE) System.out.println("File not found in cache! Returning NULL");
+			return false;
+		}
+
+		if (DEBUG_MODE) System.out.println("File found, checking for proper mode");
 		
 		// is this file in the correct mode for updates? (must have an owner)
 		ConnectedClient owningClient = file.getOwner();
+		if (DEBUG_MODE) {
+			if (owningClient != null) {
+				System.out.println("File currently owned by " + owningClient.getClientIPName());
+			} else {
+				System.out.println("File not owned by any client - returning FALSE");
+			}
+		}
+
 		if (owningClient == null) return false; 	// nobody owns this file yet - uploading is not valid
 
 		// is the client attempting to perform the upload the real owner of the file?
-		if (!clientIPName.equalsIgnoreCase(owningClient.getClientIPName())) return false;
+		if (!clientIPName.equalsIgnoreCase(owningClient.getClientIPName())) {
+			if (DEBUG_MODE) System.out.println("Client attempting to upload is not the owner of the file!");
+			return false;
+		}
 		
 		// set new file contents
+		if (DEBUG_MODE) System.out.println("Permissions/State check okay - setting new file contents");
 		file.setFileContents(contents);
 		
 		return true;
