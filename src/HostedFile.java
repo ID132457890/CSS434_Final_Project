4b2698db3acfa7349e9106c376f00570747f4498
Matthew Sell <matthew.r.sell@gmail.com>
2016-06-03 22:30:14 -0700

    Added debug messages to HostedFile

--- src/HostedFile.java
+++ src/HostedFile.java
@@ -36,7 +36,9 @@ public class HostedFile {
 	 * @param filename The filename of the file to read/track/manage
 	 */
 	public HostedFile(String filename) {
-		
+	
+		if (FileServer.DEBUG_MODE) System.out.println("Creating HostedFile for " + filename);
+
 		// how the client referred to the file
 		this.filename = filename;
 		
@@ -45,13 +47,18 @@ public class HostedFile {
 		
 		// read file contents if possible
 		try {
+			
+			if (FileServer.DEBUG_MODE) System.out.println("Attempting to read file contents...");
 
 			fileContents = new FileContents(Files.readAllBytes(file));
+
+			if (FileServer.DEBUG_MODE) System.out.println("File contents read!");
 
 		} catch (IOException e) {
 
 			// Unable to read file contents!
-			
+			if (FileServer.DEBUG_MODE) System.out.println("Unable to read file contents: " + e.getMessage());
+
 			// Swallowing exceptions is bad. But, in this case, what else to do?
 			
 		}
@@ -63,7 +70,11 @@ public class HostedFile {
 	 * @param clientIPName The hostname/IP address of the client to de-register
 	 */
 	public void deRegisterClient(String clientIPName) {
+
+		if (FileServer.DEBUG_MODE) System.out.println("Deregistering client " + clientIPName + " from " + filename);
+
 		clients.remove(clientIPName);
+		
 	}
 
 	/**
@@ -89,18 +100,25 @@ public class HostedFile {
 	 */
 	public ConnectedClient getOwner() {
 		
+		if (FileServer.DEBUG_MODE) System.out.println("Attempting to locate owner of " + filename);
+
 		for (Entry<String, ConnectedClient> client : clients.entrySet()) {
 			
 			ServerFileState clientState = client.getValue().getFileAccessMode();
 
 			// "write_shared" or "ownership_change" is an indication of ownership
 			if (clientState == ServerFileState.WRITE_SHARED || clientState == ServerFileState.OWNERSHIP_CHANGE) {
+
+				if (FileServer.DEBUG_MODE) System.out.println("Found owner (" + client.getKey() + ") having mode " + clientState);
+
 				return client.getValue();
+			
 			}
 			
 		}
 		
 		// no owner exists
+		if (FileServer.DEBUG_MODE) System.out.println("No owner found");
 		return null;
 		
 	}
@@ -120,6 +138,8 @@ public class HostedFile {
 	 */
 	private boolean invalidateClients() {
 
+		if (FileServer.DEBUG_MODE) System.out.println("Attempting to invalidate all client copies of " + filename);
+
 		boolean operationSuccess = true;
 		
 		// iterate over collection of connected clients
@@ -128,8 +148,11 @@ public class HostedFile {
 			// ... invalidate each
 			boolean invalidateSuccess = fileClient.invalidate();
 			
+			if (FileServer.DEBUG_MODE) System.out.println("Invalidation request for " + fileClient.getClientIPName() + " results " + invalidateSuccess);
+
 			// update access mode for this client
 			if (!invalidateSuccess) {
+				if (FileServer.DEBUG_MODE) System.out.println("Invalidation failure - forcing to NOT_SHARED");
 				fileClient.setFileAccessMode(ServerFileState.NOT_SHARED);
 				operationSuccess = false;	// this operation failed
 			}
@@ -138,6 +161,8 @@ public class HostedFile {
 
 		trimClients();
 		
+		if (FileServer.DEBUG_MODE) System.out.println("Overall success of all client invalidations: " + operationSuccess);
+
 		return operationSuccess;
 		
 	}
@@ -150,18 +175,27 @@ public class HostedFile {
 	 */
 	private void registerClient(String clientIPName, int port, ServerFileState clientFileState) {
 		
+		if (FileServer.DEBUG_MODE) System.out.println("Registering client " + clientIPName + " on port " + port + " with new file state of " + clientFileState);
+		
 		// if client is already using this file, get it
 		ConnectedClient client = clients.get(clientIPName);
 		
 		// client not using this file
 		if (client == null) {
-			
+
+			if (FileServer.DEBUG_MODE) System.out.println("Client was not previously using this file, adding to list");
+
 			client = new ConnectedClient(clientIPName, port);
 			clients.put(clientIPName, client);
 			
+		} else {
+			
+			if (FileServer.DEBUG_MODE) System.out.println("Client is already using this file");
+
 		}
 
 		// set access mode
+		if (FileServer.DEBUG_MODE) System.out.println("Setting client access mode to " + clientFileState);
 		client.setFileAccessMode(clientFileState);
 
 	}
@@ -173,23 +207,32 @@ public class HostedFile {
 	 * @return Operation success (TRUE) or failure (FALSE)
 	 */
 	public boolean registerOwner(String clientIPName, int port) {
-		
+
+		if (FileServer.DEBUG_MODE) System.out.println("Setting client " + clientIPName + " at port " + port + " as owner of " + filename);
+
 		// is there an owner already?
+		if (FileServer.DEBUG_MODE) System.out.println("Finding file owner...");
 		ConnectedClient owner = getOwner();
 		if (owner == null) {
 			
+			if (FileServer.DEBUG_MODE) System.out.println("No other client owns this file");
+
 			// this client is now the owner
+			if (FileServer.DEBUG_MODE) System.out.println("Setting file state to WRITE_SHARED");
 			fileState = ServerFileState.WRITE_SHARED;
 			
 			// register this client, write mode
 			registerClient(clientIPName, port, ServerFileState.WRITE_SHARED);
 			
 			// success!
+			if (FileServer.DEBUG_MODE) System.out.println("Client is registered as owner");
 			return true;
 			
 		}
 		
 		// this client is not the owner
+		if (FileServer.DEBUG_MODE) System.out.println("Current owner is: " + owner.getClientIPName());
+		if (FileServer.DEBUG_MODE) System.out.println("Setting file state to OWNERSHIP_CHANGE");
 		fileState = ServerFileState.OWNERSHIP_CHANGE;
 		
 		// tell the owner to write back its' changes
@@ -197,6 +240,7 @@ public class HostedFile {
 		if (!writebackSuccess) return false;	// operation failed, file status indeterminate (retain)
 		
 		// take ownership now
+		if (FileServer.DEBUG_MODE) System.out.println("Setting file state to WRITE_SHARED");
 		fileState = ServerFileState.WRITE_SHARED;
 		
 		// register this client, write mode
@@ -214,8 +258,16 @@ public class HostedFile {
 	 */
 	public void registerReader(String clientIPName, int port) {
 		
+		if (FileServer.DEBUG_MODE) System.out.println("Registering client " + clientIPName + " at port " + port + " as a reader");
+
 		// update file state if this is the first client to register
-		if (fileState == ServerFileState.NOT_SHARED) fileState = ServerFileState.READ_SHARED;
+		if (fileState == ServerFileState.NOT_SHARED) {
+			
+			if (FileServer.DEBUG_MODE) System.out.println("Changing file state from NOT_SHARED to READ_SHARED");
+
+			fileState = ServerFileState.READ_SHARED;
+
+		}
 		
 		// register this client, read-only
 		registerClient(clientIPName, port, ServerFileState.READ_SHARED);
@@ -230,6 +282,8 @@ public class HostedFile {
 	
 		try {
 			
+			if (FileServer.DEBUG_MODE) System.out.println("Attempting to overwrite " + filename);
+
 			// write back changes to file on filesystem
 			Files.write(file, fileContents.get(), new OpenOption[]{});
 
@@ -241,12 +295,13 @@ public class HostedFile {
 			if (!invalidationSuccess) return;
 			
 			// this file is not shared any more
+			if (FileServer.DEBUG_MODE) System.out.println("Setting file state to NOT_SHARED");
 			fileState = ServerFileState.NOT_SHARED;
 
 		} catch (IOException e) {
 
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			// Not sure what else to do with this
+			if (FileServer.DEBUG_MODE) System.out.println("Caught exception trying to update file: " + e.getMessage());
 		
 		}
 	
@@ -257,13 +312,21 @@ public class HostedFile {
 	 */
 	private void trimClients() {
 		
+		if (FileServer.DEBUG_MODE) System.out.println("Cleaning up list of clients sharing this file");
+
 		// iterate through all connected clients
 		Iterator<ConnectedClient> fileClients = clients.values().iterator();
 		while (fileClients.hasNext()) {
 		
 			// ... and remove those not sharing the file
 			ConnectedClient client = fileClients.next();
-			if (client.getFileAccessMode() == ServerFileState.NOT_SHARED) fileClients.remove();
+			if (client.getFileAccessMode() == ServerFileState.NOT_SHARED) {
+				
+				if (FileServer.DEBUG_MODE) System.out.println("Removing " + client.getClientIPName() + " from the list");
+
+				fileClients.remove();
+
+			}
 			
 		}
 		
